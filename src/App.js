6fa3f5ed0dd95e7246fd90dcee000436d53a56b5
chris-yi <kri5to.yi@gmail.com>
2017-12-04 16:31:38 -0700

    home page edit

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Weight from "./Components/Weight/Weight"
 import Analysis from "./Components/Analysis/Analysis"
 import Profile from "./Components/Profile/Profile"
 
+
 class App extends Component {
 
   render() {
