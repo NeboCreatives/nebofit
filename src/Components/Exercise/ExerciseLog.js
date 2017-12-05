253c23043ceebe020eaf0e426ca6b51de70abe7a
snguyen618 <snguyen618@gmail.com>
2017-12-05 16:58:51 -0700

    exercise log updated

--- src/Components/Exercise/ExerciseLog.js
+++ src/Components/Exercise/ExerciseLog.js
@@ -1,41 +1,64 @@
 import React, {Component} from 'react';
-import DropDownMenu from 'material-ui/DropDownMenu';
-import MenuItem from 'material-ui/MenuItem';
 import {connect} from 'react-redux';
 import {getExercises} from '../../ducks/exerciseReducer';
 
-class ExerciseLog extends Component{
-    constructor(){
-        super()
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
 
-        this.state = {value: 1}
-    }
 
-    handleChange = (event,index, value) => {
-        this.setState({value})
-    }
+class ExerciseLog extends Component {
+  constructor() {
+    super();
 
-    render(){
-        const exercises = []
-        const exercisesList = this.props.exercises.map((item,index)=>{
-            exercises.push(<MenuItem value={index} key={index} primaryText={item}/>)
-        })
-        return(
-          <div>
-    <DropDownMenu style={{width:"200px"}} maxHeight={300} value={this.state.value} onChange={this.handleChange}>
-        {exercises}
-      </DropDownMenu>
-          </div>  
-        )
-    }
+    this.state = {value: 1};
+  }
+
+  handleDropDown = (event, index, value) => {
+    this.setState({value});
+  };
+
+  render() {
+    const exercises = [];
+    const exercisesList = this.props.exercises.map((item, index) => {
+      exercises.push(<MenuItem value={index} key={index} primaryText={item}/>);
+    });
+    return (
+      <div style={{display: 'flex', alignItems: 'center'}}>
+        <DropDownMenu style={{width: '200px'}} maxHeight={300} value={this.state.value} onChange={this.handleDropDown}>
+          {exercises}
+        </DropDownMenu>
+
+        <div style={{margin: '0 6px 0 6px'}}>SETS</div>
+        <input
+          ref='sets'
+          placeholder='0'
+        />
+
+        <div style={{margin: '0 6px 0 6px'}}>REPS</div>
+        <input
+          ref='reps'
+          placeholder='0'
+        />
+
+        <div style={{margin: '0 6px 0 6px'}}>RPE</div>
+        <input
+          ref='rpe'
+          placeholder='0'
+        />
+        <button>
+          Add
+        </button>
+      </div>
+    );
+  }
 }
 
 const mapsStateToProps = (state) => {
-    const {exercises} = state.reducer
-    console.log(exercises)
-    return {
-        exercises
-    }
-}
+  const {exercises} = state.reducer;
+  console.log(exercises);
+  return {
+    exercises,
+  };
+};
 
-export default connect(mapsStateToProps, {getExercises}) (ExerciseLog);
+export default connect(mapsStateToProps, {getExercises})(ExerciseLog);
