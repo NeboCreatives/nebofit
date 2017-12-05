1197ccd98297c42afc568c033d3501d3ee7d73e0
snguyen618 <snguyen618@gmail.com>
2017-12-04 20:36:27 -0700

    redux boilerplate implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import axios from 'axios';
 import { Route } from "react-router-dom";
 
 // ----- Component Imports ----- //
---
+++ src/store.js
@@ -0,0 +1,9 @@
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import promiseMiddleware from 'redux-promise-middleware';
+import reducer from '.ducks/reducer'
+
+const reducers = combineReducers({
+  reducer,
+});
+
+export default createStore(reducers, applyMiddleware(promiseMiddleware()));
