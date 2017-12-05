28518e177de758f0a50d230b1c06c8eb2dd741f4
Travis Meamber <travismeamber@Traviss-MacBook-Pro.local>
2017-12-05 13:32:08 -0700

    npm material ui and import

---
+++ src/Components/Exercise/ExerciseLog.js
@@ -0,0 +1,18 @@
+import React, {Copmponent} from 'react';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
+
+class ExerciseLog extends Component{
+    
+
+
+    render(){
+        return(
+          <div>
+
+          </div>  
+        )
+    }
+}
+
+export default ExerciseLog;
