aa971347862f101a4ed43437be6a6f0353619547
Travis Meamber <travismeamber@Traviss-MacBook-Pro.local>
2017-12-05 14:19:16 -0700

    drop down menu displays

--- src/Components/Exercise/ExerciseLog.js
+++ src/Components/Exercise/ExerciseLog.js
@@ -1,18 +1,41 @@
-import React, {Copmponent} from 'react';
+import React, {Component} from 'react';
 import DropDownMenu from 'material-ui/DropDownMenu';
 import MenuItem from 'material-ui/MenuItem';
+import {connect} from 'react-redux';
+import {getExercises} from '../../ducks/exerciseReducer';
 
 class ExerciseLog extends Component{
-    
+    constructor(){
+        super()
 
+        this.state = {value: 1}
+    }
+
+    handleChange = (event,index, value) => {
+        this.setState({value})
+    }
 
     render(){
+        const exercises = []
+        const exercisesList = this.props.exercises.map((item,index)=>{
+            exercises.push(<MenuItem value={item} key={index} primaryText={item}/>)
+        })
         return(
           <div>
-
+    <DropDownMenu maxHeight={300} value={this.state.value} onChange={this.handleChange}>
+        {exercises}
+      </DropDownMenu>
           </div>  
         )
     }
 }
 
-export default ExerciseLog;
+const mapsStateToProps = (state) => {
+    const {exercises} = state.reducer
+    console.log(exercises)
+    return {
+        exercises
+    }
+}
+
+export default connect(mapsStateToProps, {getExercises}) (ExerciseLog);
--- src/ducks/exerciseReducer.js
+++ src/ducks/exerciseReducer.js
@@ -1,10 +1,25 @@
 const initialState = {
-  data: [],
+  exercises: [ 
+    'benchpress',
+    'squats',
+    'pull-ups',
+    'sit-ups'
+  ],
 };
 
+const GET_EXERCISES = 'GET_EXERCISES'
+
+export const getExercises = (exercises) => {
+  return {
+    type: GET_EXERCISES,
+    payload: exercises
+  }
+}
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
+    case GET_EXERCISES + '_FULLFILLED':
+    return Object.assign({}, state, {exercises: action.payload})
     default:
       return state;
   }
