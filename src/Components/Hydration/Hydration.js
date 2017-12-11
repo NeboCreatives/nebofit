bfc1fe8e657821715cb55f3294f44c8ee867418e
snguyen618 <snguyen618@gmail.com>
2017-12-11 15:27:19 -0700

    weekly totals and averages added to hydration and steps

--- src/Components/Hydration/Hydration.js
+++ src/Components/Hydration/Hydration.js
@@ -1,60 +1,62 @@
-import React, { Component } from "react";
-import "../../Details.css";
+import React, {Component} from 'react';
+import '../../Details.css';
 import moment from 'moment';
-import { Circle } from 'rc-progress';
-import {Bar} from "react-chartjs-2"
+import {Circle} from 'rc-progress';
+import {Bar} from 'react-chartjs-2';
 import {connect} from 'react-redux';
 import {getTodayNutrition} from '../../ducks/databaseReducer';
-import Hamburger from "../Hamburger/Hamburger"
+import Hamburger from '../Hamburger/Hamburger';
 
 
 class Hydration extends Component {
-  constructor(){
-    super()
+  constructor() {
+    super();
 
-this.state = {
-  pulse: null,
-  percent: 0
-}
+    this.state = {
+      pulse: null,
+      percent: 0,
+    };
 
-this.percentAnimation = this.percentAnimation.bind(this)
+    this.percentAnimation = this.percentAnimation.bind(this);
 
   }
-  componentDidMount(){
+
+  componentDidMount() {
     this.setState({
-      pulse: setInterval(this.percentAnimation, 12)
-    })
- }
+      pulse: setInterval(this.percentAnimation, 12),
+    });
+  }
 
- killInterval(){
-    clearInterval(this.state.pulse)
- }
+  killInterval() {
+    clearInterval(this.state.pulse);
+  }
 
- percentAnimation(){
-  let hydration = ((Math.round(this.props.todayData.todayNutrition.water * 0.033814022558919)) / this.props.userData.goal_hydration) * 100;
-  if (this.state.percent < hydration){
-     this.setState({
-       percent: ++this.state.percent
-})
-} else {
-  this.killInterval()
-  console.log(this.state.pulse)
-}
+  percentAnimation() {
+    let hydration = ((Math.round(this.props.todayData.todayNutrition.water * 0.033814022558919)) / this.props.userData.goal_hydration) * 100;
+    if (this.state.percent < hydration) {
+      this.setState({
+        percent: ++this.state.percent,
+      });
+    } else {
+      this.killInterval();
+      console.log(this.state.pulse);
+    }
 
-}
+  }
 
   render() {
     let mapWater = this.props.allData.nutritionData.map(dayData => {
-      return Math.round(dayData.water*0.033814022558919)
-    })
+      return Math.round(dayData.water * 0.033814022558919);
+    });
 
     let mapDays = this.props.allData.nutritionData.map(dayData => {
-      return moment(dayData.date).format('dddd')
-    })
-    mapWater = mapWater.splice(0,7).reverse();
-    mapDays = mapDays.splice(0,7).reverse();
+      return moment(dayData.date).format('dddd');
+    });
+    let weeklyAvg = Math.round(mapWater.slice(0, 7).reduce((a, b) => a + b) / 7);
+    mapWater = mapWater.splice(0, 7).reverse();
+    mapDays = mapDays.splice(0, 7).reverse();
 
-    console.log(mapWater)
+    console.log(weeklyWater);
 
     let data = {
       labels: mapDays,
@@ -63,23 +65,23 @@ this.percentAnimation = this.percentAnimation.bind(this)
           // backgroundColor: 'rgb(114, 118, 231)',
           borderColor: 'rgb(95,197,212)',
           borderWidth: 3,
-          data: mapWater
-        }
-      ]
-    }
-    console.log(this.state.percent)
+          data: mapWater,
+        },
+      ],
+    };
+    console.log(this.state.percent);
     return (
       <div className="Details">
-      <Hamburger/>
+        <Hamburger/>
         <div className="Details_Header">
           <div>
-          <i className="fa fa-tint" aria-hidden="true"></i>
+            <i className="fa fa-tint" aria-hidden="true"></i>
             <h1 className="Details_Today">Hydration</h1>
           </div>
           <div className='Details_Header_Buffer'></div>
         </div>
 
-        <hr />
+        <hr/>
         <div className="Details_Main_Container">
           <div className="Details_Metric">
             <div className="Details_RC_Container">
@@ -94,15 +96,16 @@ this.percentAnimation = this.percentAnimation.bind(this)
                   />
                   <div className="Details_Chart_Details">
                     <i className="fa fa-sort-asc" aria-hidden="true">
-                      {" "}
+                      {' '}
                       +1
                     </i>
-                    <p>{(Math.round(this.props.todayData.todayNutrition.water*0.033814022558919))}</p>
+                    <p>{(Math.round(this.props.todayData.todayNutrition.water * 0.033814022558919))}</p>
                     <p>oz</p>
                   </div>
                 </div>
                 <div className="Details_Goal_Reminder">
-                  <h1>{Math.round(this.props.userData.goal_hydration - this.props.todayData.todayNutrition.water * 0.033814022558919)} oz left</h1>
+                  <h1>{Math.round(this.props.userData.goal_hydration - this.props.todayData.todayNutrition.water * 0.033814022558919)} oz
+                    left</h1>
                 </div>
               </div>
             </div>
@@ -118,18 +121,21 @@ this.percentAnimation = this.percentAnimation.bind(this)
                       yAxes: [
                         {
                           ticks: {
-                            beginAtZero: true
-                          }
-                        }
-                      ]
+                            beginAtZero: true,
+                          },
+                        },
+                      ],
                     },
                     legend: {
-                      display: false
-                    }
+                      display: false,
+                    },
                   }}
                 />
               </div>
             </div>
+          </div>
+          <div>
+            <div>{`Weekly Average: ${weeklyAvg}`}</div>
           </div>
         </div>
       </div>
@@ -139,12 +145,12 @@ this.percentAnimation = this.percentAnimation.bind(this)
 
 
 const mapStateToProps = (state) => {
-  const {todayData, userData, allData} = state.databaseReducer
+  const {todayData, userData, allData} = state.databaseReducer;
   return {
     todayData,
     userData,
-    allData
-  }
-}
+    allData,
+  };
+};
 
 export default connect(mapStateToProps, {getTodayNutrition})(Hydration);
